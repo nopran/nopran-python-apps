186c01dbd9a829203b3ed4a927c2359bc289b119
Muhammad Nopran <noprantor@gmail.com>
2023-11-27 14:46:55 +0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,14 +1,6 @@
 import streamlit as st
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-# Pengaturan Style dan Palet Warna
-sns.set(style="whitegrid")
-palette = sns.color_palette("viridis")
-
-# Judul Aplikasi
-st.title('Optimasi Pertumbuhan Tanaman: Analisis Kondisi Lingkungan')
+import plotly.express as px
 
 # Fungsi untuk Memuat Data
 @st.cache
@@ -16,56 +8,35 @@ def load_data():
     data = pd.read_csv('sf.csv')
     return data
 
-# Fungsi untuk Membuat Plot
-def plot_dist(column, title):
-    fig, ax = plt.subplots()
-    sns.histplot(data[column], kde=True, bins=30, color=palette[2], ax=ax)
-    ax.set_title(title, fontsize=16)
-    ax.set_xlabel(column, fontsize=14)
-    ax.set_ylabel('Frekuensi', fontsize=14)
-    # Menambahkan Anotasi
-    mean_val = data[column].mean()
-    ax.axvline(mean_val, color='red', linestyle='--')
-    ax.text(mean_val+0.5, 10, f'Rata-rata: {mean_val:.2f}', color='red')
-    return fig
-
 # Memuat Data
 data = load_data()
 
-# Bagian: Menampilkan Data Mentah
+# Judul Aplikasi
+st.title('Optimasi Pertumbuhan Tanaman: Analisis Kondisi Lingkungan')
+
+# Menampilkan Data dan Penjelasan
 st.write("### Data Mentah")
 st.dataframe(data.head())
 
-# Bagian: Penjelasan Data
 st.write("### Penjelasan Data")
-st.markdown("""
-Ini adalah data yang berkaitan dengan kondisi pertanian, mencakup:
-- **N, P, K**: Nutrisi tanah
-- **Temperature**: Suhu lingkungan
-- **Humidity**: Kelembaban
-- **pH**: Tingkat keasaman tanah
-- **Rainfall**: Curah hujan
-- **Label**: Jenis tanaman
-""")
+# Tambahkan penjelasan data Anda di sini
 
-# Bagian: Visualisasi Data
+# Pilihan Kolom untuk Visualisasi
+col_options = list(data.columns)
+selected_col = st.selectbox('Pilih Kolom untuk Visualisasi', col_options)
+
+# Slider untuk Jumlah Bins
+bins = st.slider('Pilih Jumlah Bins', min_value=10, max_value=100, value=30)
+
+# Fungsi untuk Membuat Plot dengan Plotly
+def plot_dist_plotly(column, bins):
+    fig = px.histogram(data, x=column, nbins=bins, title=f'Distribusi {column}')
+    fig.update_layout(bargap=0.1)
+    return fig
+
+# Visualisasi Data dengan Plotly
 st.write("### Visualisasi Data")
-
-# Distribusi Suhu
-st.write("#### Distribusi Suhu")
-st.pyplot(plot_dist('temperature', 'Distribusi Suhu'))
-
-# Distribusi Kelembaban
-st.write("#### Distribusi Kelembaban")
-st.pyplot(plot_dist('humidity', 'Distribusi Kelembaban'))
-
-# Distribusi pH
-st.write("#### Distribusi pH")
-st.pyplot(plot_dist('ph', 'Distribusi pH'))
-
-# Distribusi Curah Hujan
-st.write("#### Distribusi Curah Hujan")
-st.pyplot(plot_dist('rainfall', 'Distribusi Curah Hujan'))
+st.plotly_chart(plot_dist_plotly(selected_col, bins))
 
 # Footer
 st.write("Dibuat oleh [Nama Anda]")
